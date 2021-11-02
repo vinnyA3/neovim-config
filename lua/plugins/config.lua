9e1862eeb7ad1c11f203bb2a974c1b42112d93f9
vinnyA3 <vincent.aceto@gmail.com>
2021-11-01 22:39:40 -0400

    feat(plugins): config - cmp buffer opts
    Add option to complete from all visible buffers

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -75,7 +75,20 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'path' },
-    { name = 'buffer' }
+    {
+      name = 'buffer',
+      opts = {
+        get_bufnrs = function()
+          local bufs = {}
+
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            bufs[vim.api.nvim_win_get_buf(win)] = true
+          end
+
+          return vim.tbl_keys(bufs)
+        end
+      }
+    }
   },
   mapping = {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
