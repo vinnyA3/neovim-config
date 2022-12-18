5c165ad12c20f8f25bf38fce0840551254d0dd49
vinnyA3 <vincent.aceto@gmail.com>
2022-12-18 00:23:08 -0500

    style(init): remove old comments

--- init.lua
+++ init.lua
@@ -1,17 +1,2 @@
--- local set_keymap = vim.api.nvim_set_keymap
---
--- -- setup leader key first and foremost
--- set_keymap('n', '<Space>', '', {})
--- set_keymap('v', '<Space>', '', {})
---
--- vim.g.mapleader = " "
---
--- require('core/options')
--- require('core/keybindings')
--- require('core/disabled') -- disabled vim default plugings
---
--- require('plugins/init') -- packer init plugins
--- require('plugins/config') -- source file to include local plugin config
---
--- require('lsp/init')
+-- E N T R Y
 require('vin')
