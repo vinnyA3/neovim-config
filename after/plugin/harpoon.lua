f7e5a2de4e3f370859d6ead6b71c7e205f5144cb
vinnyA3 <vincent.aceto@gmail.com>
2022-12-19 00:23:31 -0500

    feat(harpoon): nicer display width, better keys
    Keys are pretty nice, picked from prime's defaults.  Also, set a better
    width display for the UI

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,10 +1,6 @@
 local map = require('vin.utils').map
 
-require('harpoon').setup({
-  menu = {
-    width = vim.api.nvim_win_get_width(0) - 4,
-  }
-})
+require('harpoon').setup{}
 
-map("n", "<leader>ha", ":lua require'harpoon.mark'.add_file()<CR>")
-map("n", "<leader>hh", ":lua require'harpoon.ui'.toggle_quick_menu()<CR>")
+map("n", "<leader>a", ":lua require'harpoon.mark'.add_file()<CR>")
+map("n", "<C-e>", ":lua require'harpoon.ui'.toggle_quick_menu()<CR>")
