c65505c4c91f3ec1ea88ac2acb7f8c5902067dba
vinnyA3 <vincent.aceto@gmail.com>
2023-12-28 00:41:04 -0500

    feat(plugin): blankline - update config

--- after/plugin/blankline.lua
+++ after/plugin/blankline.lua
@@ -1,22 +1,10 @@
-vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_filetype = {
-  'lua',
-  'javascript',
-  'javascriptreact',
-  'typescript',
-  'typescriptreact',
-  'html',
-  'nix',
-  'rust',
-  'svelte',
-  'css',
-  'scss',
-  'c',
-  'python',
-}
-
-require('indent_blankline').setup {
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
+require('ibl').setup {
+   scope = {
+     enabled = true,
+     show_start = true,
+     show_end = true,
+     injected_languages = true,
+     highlight = { "Function", "Label" },
+     priority = 500,
+   }
 }
