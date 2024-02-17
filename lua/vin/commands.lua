d2fc90b31950d75e58e8e70c3d4f98db29b167e8
vinnyA3 <vincent.aceto@gmail.com>
2024-02-16 23:52:03 -0500

    feat(commands): add helpful command for eslint
    This command pipe eslint to vim's make which ensures that the process
    runs on all files & dumps error output to the quickfix list

--- lua/vin/commands.lua
+++ lua/vin/commands.lua
@@ -1,8 +1,17 @@
--- Plugin Dependent User Commands
+-- run :make to run typescript compiler on all project files & pipe errors to QL
+local augroup = vim.api.nvim_create_augroup("strdr4605", { clear = true })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "typescript,typescriptreact",
+  group = augroup,
+  command = "compiler tsc | setlocal makeprg=npx\\ tsc",
+})
+
+-- Plugin Dependent User Commands
 -- Load Snippets (requires luasnip)
 vim.api.nvim_create_user_command(
   'Snippets',
   'lua require("luasnip.loaders.from_snipmate").load()',
   {}
 )
+
